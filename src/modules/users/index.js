2804a0d8ba5135f12c8e3b3af3a28bca2728c5c4
Nelson jr <nelsonjr.dev@gmail.com>
2025-06-02 14:53:53 -0300

    feat: users crud

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -24,15 +24,28 @@ export const create = async ctx => {
   }
 }
 export const update = async ctx => {
-  const user = await prisma.user.update({
-    where: { id: ctx.params.id },
-    data: ctx.request.body,
-  })
-  ctx.body = 'List of users'
+  try {
+    const { name, email, password } = ctx.request.body
+    const user = await prisma.user.update({
+      where: { id: ctx.params.id },
+      data,
+    })
+    ctx.body = user
+  } catch (err) {
+    ctx.status = 500
+    ctx.body = 'Ups! Something went wrong'
+    return
+  }
 }
 export const remove = async ctx => {
-  const user = await prisma.user.delete({
-    where: { id: ctx.params.id },
-  })
-  ctx.body = 'List of users'
+  try {
+    const user = await prisma.user.delete({
+      where: { id: ctx.params.id },
+    })
+    ctx.body = { id: ctx.params.id, message: 'User deleted successfully' }
+  } catch (err) {
+    ctx.status = 500
+    ctx.body = 'Ups! Something went wrong'
+    return
+  }
 }
