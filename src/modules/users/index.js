cd4224e968115364f06cf6bb97ccbf88bd925fb7
Nelson jr <nelsonjr.dev@gmail.com>
2025-06-02 11:12:50 -0300

    feat: create user schema and migration

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -1,12 +1,38 @@
-export const list = ctx => {
+import { prisma } from '~/data'
+
+export const list = async ctx => {
+  try {
+    const users = await prisma.user.findMany()
+
+    ctx.body = users
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = 'Ups! Something went wrong'
+    return
+  }
+}
+export const create = async ctx => {
+  try {
+    const user = await prisma.user.create({
+      data: ctx.request.body,
+    })
+    ctx.body = user
+  } catch (err) {
+    ctx.status = 500
+    ctx.body = 'Ups! Something went wrong'
+    return
+  }
+}
+export const update = async ctx => {
+  const user = await prisma.user.update({
+    where: { id: ctx.params.id },
+    data: ctx.request.body,
+  })
   ctx.body = 'List of users'
 }
-export const create = ctx => {
-  ctx.body = 'List of users'
-}
-export const update = ctx => {
-  ctx.body = 'List of users'
-}
-export const remove = ctx => {
+export const remove = async ctx => {
+  const user = await prisma.user.delete({
+    where: { id: ctx.params.id },
+  })
   ctx.body = 'List of users'
 }
