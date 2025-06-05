1721b3f130d2181d66130a384e8dce62f0fb7a53
Nelson jr <nelsonjr.dev@gmail.com>
2025-06-05 14:06:16 -0300

    refactor: separation of decode basic token with tests

---
+++ src/modules/users/services.js
@@ -0,0 +1,41 @@
+class TokenTypeError extends Error {
+  constructor(message = 'Invalid token type') {
+    super()
+    this.message = message
+    this.custom = true
+  }
+}
+
+class Base64EncodedError extends Error {
+  constructor(message = 'Wrong credentials is not correct encodedclea') {
+    super()
+    this.message = message
+    this.custom = true
+  }
+}
+
+class BadCredentialError extends Error {
+  constructor(message = 'Wrong credentials format') {
+    super()
+    this.message = message
+    this.custom = true
+  }
+}
+
+export const decodeBasicToken = basicToken => {
+  const [type, credentials] = basicToken.split(' ')
+  if (type !== 'Basic') {
+    throw new TokenTypeError()
+  }
+  const decoded = Buffer.from(credentials, 'base64').toString()
+  const endocded = Buffer.from(decoded, 'utf8').toString('base64')
+
+  if (endocded !== credentials) {
+    throw new Base64EncodedError()
+  }
+
+  if (decoded.indexOf(':') === -1) {
+    throw new BadCredentialError()
+  }
+  return decoded.split(':')
+}
---
+++ src/modules/users/services.test.js
@@ -0,0 +1,53 @@
+import { decodeBasicToken } from './services'
+
+describe('User Module Tests', () => {
+  it('should return credential by basic athentication token ', () => {
+    //prepare
+    const email = 'nelson@gmail.com'
+    const password = '123456'
+    const token = Buffer.from(`${email}:${password}`, 'utf8').toString('base64')
+    const basicToken = `Basic ${token}`
+    // execute
+    const result = decodeBasicToken(basicToken)
+    // expectation
+    expect(result).toEqual([email, password])
+  })
+})
+
+it('should throw new error when token is not basic type ', () => {
+  //prepare
+  const email = 'nelson@gmail.com'
+  const password = '123456'
+  const token = Buffer.from(`${email}${password}`, 'utf8').toString('base64')
+  const basicToken = `Bearer ${token}`
+  // execute
+  const result = () => decodeBasicToken(basicToken)
+  // expectation
+  expect(result).toThrowError('Invalid token type')
+})
+
+it('Should throw a new error when credentials is not on correct format', () => {
+  //prepare
+  const email = 'nelson@gmail.com'
+  const password = '123456'
+  const token = Buffer.from(`${email}${password}`, 'utf8').toString('base64')
+  const basicToken = `Basic ${token}`
+  // execute
+  const result = () => decodeBasicToken(basicToken)
+  // expectation
+  expect(result).toThrowError('Wrong credentials format')
+})
+
+it('Should throw a new error when credentials is not base64 encoded', () => {
+  //prepare
+  const email = 'nelson@gmail.com'
+  const password = '123456'
+  const token = Buffer.from(`${email}:${password}`)
+
+  const basicToken = `Basic ${token}`
+
+  // execute
+  const result = () => decodeBasicToken(basicToken)
+  // expectation
+  expect(result).toThrowError('Wrong credentials is not correct encodedclea')
+})
