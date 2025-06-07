f7848bfa99ae6d522c8e4183ec92f9cce5fd3fcd
Nelson jr <nelsonjr.dev@gmail.com>
2025-06-06 22:21:26 -0300

    test: change the DB_URL variable to avoid using the same production database

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,9 +1,21 @@
 import request from 'supertest'
+import bcrypt from 'bcrypt'
+
+import { prisma } from '~/data'
+
 import { app } from './server-setup'
 
 const server = app.listen()
 
 describe("users routes", () => {
+  beforeAll(async () => {
+   await prisma.user.deleteMany({})
+  })
+  afterAll(async () => {
+    await prisma.$disconnect()
+    server.close()
+  })
+
   it("should return not found with wrong email", async () => {
     // prepare
       const email = 'wrong@gmail.com'
@@ -13,11 +25,10 @@ describe("users routes", () => {
      const result = await request(server).get('/login').auth(email, password)
 
     // expectation
-     console.log(result.status)
+     expect(result.status).toBe(404)
 
   })})
 
-  describe("users routes", () => {
   it("should return not found with wrong password", async () => {
     // prepare
       const email = 'nson@gmail.com'
@@ -27,6 +38,32 @@ describe("users routes", () => {
      const result = await request(server).get('/login').auth(email, password)
 
     // expectation
-     console.log(result.status)
+      expect(result.status).toBe(404)
 
-  })})
+  })
+
+   it("should return logged in user by correct credentials", async () => {
+    // prepare
+      const email = 'nson@gmail.com'
+      const password = '123456'
+
+       const saltRounds = 10
+
+          const hashedPassword = await bcrypt.hash(
+           password, saltRounds )
+
+      await prisma.user.create({
+        data: { email, password: hashedPassword },
+      })
+
+    //execute
+     const result = await request(server).get('/login').auth(email, password)
+
+    // expectation
+     expect(result.status).toBe(200)
+     expect(result.body.user).toBeTruthy()
+     expect(result.body.user.id).toBeTruthy()
+     expect(result.body.user.email).toBe(email)
+    // expect(result.body.user.password).toBeFalsy()
+
+  })
