90596d70a19ff1202cbe0df8f6536649a76b94d3
unknown <nelsonjr.dev@gmail.com>
2025-07-21 14:42:29 -0300

    fix: value prop as string from request

--- src/modules/transactions/index.js
+++ src/modules/transactions/index.js
@@ -1,12 +1,12 @@
 import { Prisma } from '@prisma/client'
-
 import * as model from './model'
 
 export const create = async ctx => {
 try{
  const transaction = await model.create({
   data: {
-    ...ctx.request.body,
+    description: ctx.request.body.description,
+    ...(ctx.request.body.value && { value: parseFloat(ctx.request.body.value) }),
     userId: ctx.auth.user.id,
   }
 })
