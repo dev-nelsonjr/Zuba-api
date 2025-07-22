dfc35f3b54d431decd24af1a009adc95aeb17649
unknown <nelsonjr.dev@gmail.com>
2025-07-21 21:45:55 -0300

    feat: delete transaction endpoint

--- src/modules/transactions/model.js
+++ src/modules/transactions/model.js
@@ -2,4 +2,5 @@ import {prisma} from '../../data'
 
 export const create = prisma.transaction.create
 export const findMany = prisma.transaction.findMany
-export const update = prisma.transaction.updateMany
+export const updateMany = prisma.transaction.updateMany
+export const removeMany = prisma.transaction.deleteMany
