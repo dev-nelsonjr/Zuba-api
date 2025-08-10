6a4ead68bc39a9da5013e4c9a783859ea6740f03
unknown <nelsonjr.dev@gmail.com>
2025-08-10 07:53:56 -0300

    feat: implement basic transaction creation module

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Router from 'koa-router'
 
 import * as users from './modules/users'
+import * as transactions from './modules/transactions'
 
 export const router = new Router()
 
@@ -49,10 +50,13 @@ router.get('/', ctx => {
   ctx.status = 200
 })
 
-
+// Auth
 router.post('/login', users.login)
 
+// Users
 router.get('/users', users.list)
 router.post('/users', users.create)
 router.put('/users/:id', users.update)
 router.delete('/users/:id', users.remove)
+
+router.post('/transactions', transactions.create)
