f33253a4744ad412c472dc05d3db2ebd6eddb64f
NelsonJr <nelsonjr.dev@gmail.com>
2026-08-14 21:17:43 -0300

    chore: configure ESLint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  {
+    ignores: ['coverage/**', 'dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+]
